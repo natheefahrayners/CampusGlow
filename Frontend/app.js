d06fc32d525d21232a228a783d74233a7bb34bc0
Natheefah Rayners <natheefahrayners4@gmail.com>
2026-02-26 11:35:56 +0200

    update

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { createApp, ref, computed, reactive, watch } = Vue;
 
 createApp({
@@ -627,32 +626,3 @@ createApp({
     };
   }
 }).mount('#app');
-
-
-=======
-import express from "express";
-import cors from "cors";
-import productRoutes from "./routes/productRoutes.js";
-import cartRoutes from "./routes/cartRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
-import discountRoutes from "./routes/discountRoutes.js";
-import reviewRoutes from "./routes/reviewRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/products", productRoutes);
-app.use("/api/carts", cartRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/payments", paymentRoutes);
-app.use("/api/discounts", discountRoutes);
-app.use("/api/reviews", reviewRoutes);
-app.use("/api/auth", authRoutes);
-
-export default app;
->>>>>>> S
-
